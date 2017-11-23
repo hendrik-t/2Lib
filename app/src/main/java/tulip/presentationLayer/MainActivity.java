fdf82ffde4e82b573523dc24a7bcfdee277759a6
Hendrik <hendrik-t@t-online.de>
2017-11-23 13:19:12 +0100

    Added MainActivity Class Header

--- app/src/main/java/tulip/presentationLayer/MainActivity.java
+++ app/src/main/java/tulip/presentationLayer/MainActivity.java
@@ -3,6 +3,13 @@ package tulip.presentationLayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* Shows the Main Activity.
+ * Author : Hendrik Tete
+ * Environment : JDK 1.8, Windows 10
+ * File created : 23.11.2017
+ * Last edited : 23.11.2017
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
