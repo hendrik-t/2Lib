b181536805b57a22055db91feb716645a700bad3
Hendrik <hendrik-t@t-online.de>
2017-11-24 11:40:02 +0100

    Created empty List Classes

---
+++ app/src/main/java/dataAccessObjects/BookList.java
@@ -0,0 +1,11 @@
+package dataAccessObjects;
+
+/* The Class for a Table of the template "Books".
+ * Author : Hendrik Tete
+ * Environment : JDK 1.8, Windows 10
+ * File created : 24.11.2017
+ * Last edited : 24.11.2017
+ */
+
+public class BookList extends TableList {
+}
---
+++ app/src/main/java/dataAccessObjects/MusicList.java
@@ -0,0 +1,11 @@
+package dataAccessObjects;
+
+/* The Class for a Table of the template "Music".
+ * Author : Hendrik Tete
+ * Environment : JDK 1.8, Windows 10
+ * File created : 24.11.2017
+ * Last edited : 24.11.2017
+ */
+
+public class MusicList extends TableList {
+}
