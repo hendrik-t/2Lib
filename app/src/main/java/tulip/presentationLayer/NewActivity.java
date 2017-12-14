aabaee09412288f338b1980a179ad17b8aefd12c
Nils Beck <Nilskjellbeck@aol.com>
2017-12-14 14:02:40 +0100

    - added a barcodescanner - added a new activity to start it

---
+++ app/src/main/java/tulip/presentationLayer/NewActivity.java
@@ -0,0 +1,53 @@
+package tulip.presentationLayer;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import static android.Manifest.permission.CAMERA;
+
+/**
+ * Created by nilskjellbeck on 14.12.17.
+ */
+
+public class NewActivity extends Activity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView zXingScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        zXingScannerView = new ZXingScannerView(getApplicationContext());
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
+        zXingScannerView.resumeCameraPreview(this);
+    }
+
+}
