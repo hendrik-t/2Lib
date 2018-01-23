c7554bbd499441b73689e3a9d9f925f8185d07c1
Nils Beck <Nilskjellbeck@aol.com>
2018-01-23 10:48:06 +0100

    - adapter implemented

--- app/src/main/java/tulip/presentationLayer/ListActivity.java
+++ app/src/main/java/tulip/presentationLayer/ListActivity.java
@@ -2,11 +2,16 @@ package tulip.presentationLayer;
 
 import android.app.*;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class ListActivity extends Activity {
 
     /** Attributes **/
+
+    ListView simpleList;
+    String countryList[] = {"India","China","GG"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,12 @@ public class ListActivity extends Activity {
 
         /** Get the view from activity_main.xml **/
         setContentView(R.layout.list_activity);
+
+        simpleList = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_activity, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+
+        // TableList.getTablenames
     }
 
 }
